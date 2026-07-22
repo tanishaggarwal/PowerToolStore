3e58f82d9b682b51595b105d723fbdec731b609b
tanishaggarwal <tanishaggarwal9357@gmail.com>
2026-07-22 13:34:42 +0530

    Initialized Spring Boot Project

---
+++ src/main/java/com/example/PowerToolStore/PowerToolStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.PowerToolStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerToolStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerToolStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PowerToolStore/PowerToolStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PowerToolStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerToolStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
